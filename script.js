c0329875830baa740b845eadfedbe5b0a457b394
Rusiru Laksara <rulaksara1017@gmail.com>
2026-06-14 00:59:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function generateSecurePassword() {
     const name = document.getElementById('name').value.substring(0, 3).toUpperCase();
     const bday = document.getElementById('bday').value.substring(2);
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
-    let password = name + "-" + bday + "-";
-    for(let i=0; i<8; i++) password += chars[Math.floor(Math.random() * chars.length)];
+    if (!name || !bday) { alert("Enter details!"); return; }
+    
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    let password = `${name}-${bday}-`;
+    for(let i = 0; i < 8; i++) password += chars.charAt(Math.floor(Math.random() * chars.length));
+    
     document.getElementById('passDisplay').innerText = password;
 }
